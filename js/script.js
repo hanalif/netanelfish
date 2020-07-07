5c4826c493d1ea08582f04b425ecbe119a1b23ca
Hana <hanalif619@gmail.com>
2020-07-07 12:31:23 +0300

    video selection title and bg + hamburgerforselecti

--- js/script.js
+++ js/script.js
@@ -3,18 +3,35 @@
 let burgerBtn = document.querySelector('.hamburger-manu');
 let menuOpen = false;
 let burgerMenue = document.querySelector('.hamburger-nav');
+let hamburgerUl = document.querySelector('.hamburger-nav ul');
 
-burgerBtn.addEventListener('click', function(){
-    if(!menuOpen){
-        burgerBtn.classList.add('open');
-        burgerMenue.classList.add('show');
-        menuOpen = true;
+burgerBtn.addEventListener('click', function () {
+    if (!menuOpen) {
+        openMenu();
     }
-    else{
-        burgerBtn.classList.remove('open');
-        burgerMenue.classList.remove('show');
-        menuOpen = false;
+    else {
+        closeMenu();
     }
 })
+
+hamburgerUl.addEventListener('click', function (e) {
+    if (event.target.classList.contains('hamburger-manu-item')) {
+        closeMenu()
+    }
+})
+
+function openMenu() {
+    burgerBtn.classList.add('open');
+    burgerMenue.classList.add('show');
+    menuOpen = true;
+}
+
+function closeMenu() {
+    burgerBtn.classList.remove('open');
+    burgerMenue.classList.remove('show');
+    menuOpen = false;
+}
+
+
 
 
