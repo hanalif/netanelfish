afa35e5443cf8c941cf50d279a72ed1c28e7a781
Hana <hanalif619@gmail.com>
2020-07-08 23:15:47 +0300

    stills modal

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ let burgerBtn = document.querySelector('.hamburger-manu');
 let menuOpen = false;
 let burgerMenue = document.querySelector('.hamburger-nav');
 let hamburgerUl = document.querySelector('.hamburger-nav ul');
+let stillImageGrid = document.querySelector('.stills-images-grid');
+let stillsModal = document.querySelector('.stills-modal');
+let stillCloseBtn = document.querySelector('.stills-modal .close-btn');
+let stillModalImage = document.querySelector('.stills-modal-img');
 
 burgerBtn.addEventListener('click', function () {
     if (!menuOpen) {
@@ -35,6 +39,19 @@ function closeMenu() {
 
 document.addEventListener('DOMContentLoaded', () => {
     const players = Array.from(document.querySelectorAll('.js-player')).map(p => new Plyr(p));
+});
+
+stillImageGrid.addEventListener('click', function(e) {
+    if (e.target.classList.contains('still-image-container')) {
+        let image = e.target.querySelector('img');
+        let src = image.getAttribute("src");
+        stillsModal.style.display = 'flex';
+        stillModalImage.setAttribute('src', src);
+    }
+});
+
+stillCloseBtn.addEventListener('click', function() {
+    stillsModal.style.display = 'none';
 });
 
 
